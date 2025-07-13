b8564173ebd1ac83229710db749b47f0b97ad775
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-07-13 16:29:33 -0300

    Operadores Unarios

---
+++ Java Moderno com Spring Boot/Modulo-2_Operadores/Operadores Unarios/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        a++; // Equivalente a a = a + 1;
+        System.out.println(a); // Saída: 11
+
+        int b = 10;
+        b--;
+        System.out.println(b);
+
+        int c = 10;
+        c+= 2;
+        System.out.println(c);
+    }
+}
