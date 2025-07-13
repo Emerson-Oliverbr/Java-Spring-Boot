884f74c1f80bd0f35762e3627e418e1307ae61a3
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-07-13 13:23:24 -0300

    Aprendendo sobre os tipos primitivos

---
+++ Java Moderno com Spring Boot/Modulo-1 Variaveis/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int farinha = 2;
+        System.out.println(farinha);
+        System.out.println("A receita requer " + farinha + " xicaras de farinha" + "\n");
+        byte idade = 40;
+        short ano = 2025;
+        int populacaiCidade = 500000;
+        long populacarMundial = 7800000000L;
+        float altura = 1.69f;
+        double salario = 4500.50;
+        boolean estudante = true;
+        char inicialNome = 'E';
+
+        System.out.println("Idade = " + idade);
+        System.out.println("Ano = " + ano);
+        System.out.println("População da cidade = " + populacaiCidade);
+        System.out.println("Popução Mundial = " + populacarMundial);
+        System.out.println("Altura = " + altura);
+        System.out.println("Salario = " + salario);
+        System.out.println("Estudate = " + estudante);
+        System.out.println("Primeira letra do nome = " + inicialNome);
+    }
+}
