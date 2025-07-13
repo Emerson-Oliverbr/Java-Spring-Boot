f78f0f9dc61e39a040fb588e41e467cd3cbcd78a
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-07-13 16:15:53 -0300

    Operadores de atribuição

---
+++ Java Moderno com Spring Boot/Modulo-2_Operadores/Operadores de Atribuição/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        a +=2; // Equivalente à {a = a + 2};
+        System.out.println("Valor de a => " + a);
+
+        int b = 10;
+        b = b + 2;
+        System.out.println("Valor de b => " + b);
+
+        int c = 10;
+        c-= 2;
+        System.out.println("Valor de c => " + c);
+
+
+    }
+}
