3dbda01dc22086106cc2f534286408ba21922b49
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-07-13 22:40:45 -0300

    If/Else

---
+++ Java Moderno com Spring Boot/Modulo-3_Controle de Fluxo/If-else/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+
+        if(a > 5) {
+            System.out.println("A é maior que 5");
+        } else {
+            System.out.println("A é menor ou igual 5");
+        }
+    }
+}
