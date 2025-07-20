751146860ac5306146c0bb3185647950e94cc71a
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-07-20 16:18:33 -0300

    Aprendendo sobre pacotes

---
+++ Java Moderno com Spring Boot/Modulo-5_Principios da Poo Java/7.Pacotes/FolhaDePagamento.java
@@ -0,0 +1,8 @@
+import empresa.dados.Funcionario;
+
+public class FolhaDePagamento {
+    public static void main(String[] args) {
+
+        Funcionario funcionario = new Funcionario("Emerson", 6200.00);
+    }
+}
