77edcd8ce376c37086055070bfda27b65c13add6
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-07-20 23:54:19 -0300

    Modificador de acesso default

---
+++ Java Moderno com Spring Boot/Modulo-5_Principios da Poo Java/11.Modificador de aAcesso default/FolhaDePagamento.java
@@ -0,0 +1,8 @@
+import code85.dados.Funcionario;
+import code85.dados.TestePackage;
+
+public class FolhaDePagamento {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+    }
+}
