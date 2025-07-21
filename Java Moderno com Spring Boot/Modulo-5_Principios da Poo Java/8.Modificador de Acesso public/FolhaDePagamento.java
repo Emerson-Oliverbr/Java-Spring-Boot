a5093e77cf128e05d8108738a5ac63804479006b
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-07-20 22:42:50 -0300

    Modificador de acesso public

---
+++ Java Moderno com Spring Boot/Modulo-5_Principios da Poo Java/8.Modificador de Acesso public/FolhaDePagamento.java
@@ -0,0 +1,9 @@
+import code85.dados.Funcionario;
+
+public class FolhaDePagamento {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Emerson", 6200.00);
+        funcionario.nome = "Oliveira";
+        System.out.println("Nome: " + funcionario.nome);
+    }
+}
